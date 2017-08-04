440f811f888cf2fa9a8e52bfdbde7081c7ef34ca
rizky Kharisma <ngeng.ngengs@gmail.com>
2017-08-04 09:30:29 +0700

    Add web config

---
+++ application/config/web_config.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @author rizky Kharisma <ngeng.ngengs@gmail.com>
+ *
+ * Date: 8/4/2017
+ * Time: 1:19 AM
+ *
+ * Created by PhpStorm.
+ */
+
+$config['app_name'] = 'Todong Ban';
+$config['app_name_short'] = 'TDB';
+
